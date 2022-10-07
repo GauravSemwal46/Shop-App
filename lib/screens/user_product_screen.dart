57844d4f2ff90969b83fa241f7ea4030ddfbb1c3
gauravsemwal6@gmail.com <gauravsemwal6@gmail.com>
2022-10-07 21:58:31 +0530

    add user product page

---
+++ lib/screens/user_product_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/providers/products.dart';
+import 'package:flutter_shop_app/widgets/app_drawer.dart';
+import 'package:flutter_shop_app/widgets/user_product_item.dart';
+import 'package:provider/provider.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+  const UserProductsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final productData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      drawer: const AppDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: ListView.builder(
+          itemBuilder: (_, index) => Column(
+            children: [
+              UserProductItem(
+                title: productData.items[index].title,
+                imageUrl: productData.items[index].imageUrl,
+              ),
+              const Divider(),
+            ],
+          ),
+          itemCount: productData.items.length,
+        ),
+      ),
+    );
+  }
+}
