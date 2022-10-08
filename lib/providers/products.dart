47294350f0bb7f4ff46332e9d94898b55a250746
gauravsemwal6@gmail.com <gauravsemwal6@gmail.com>
2022-10-08 08:28:07 +0530

    add function to add and update a product

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -49,8 +49,23 @@ class Products with ChangeNotifier {
     return _items.firstWhere((prod) => prod.id == id);
   }
 
-  void addProduct() {
-    // _items.add(value);
+  void addProduct(Product product) {
+    final newProduct = Product(
+      id: DateTime.now().toString(),
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
+    _items.add(newProduct);
     notifyListeners();
+  }
+
+  void updateProduct(String id, Product product) {
+    final prodIndex = _items.indexWhere((prod) => prod.id == id);
+    if (prodIndex >= 0) {
+      _items[prodIndex] = product;
+      notifyListeners();
+    }
   }
 }
